a42b4c4e0a42cc80f05ed120d9a966462604cea5
Sreejith K <sreejithemk@gmail.com>
2011-04-08 18:41:55 +0530

    Makefile added.

---
+++ Makefile
@@ -0,0 +1,33 @@
+# Makefile to compile Scatch Tool for Tuxpaint
+# @author: Sreejith K <sreejithemk@gmail.com>
+# Created on 8th Apr 2011
+
+CC := gcc
+SDL_LIB_PATH := /usr/include/SDL
+TP_LIB_PATH := /usr/local/include/tuxpaint
+TP_PLUGIN_DIR := /usr/local/lib/tuxpaint/plugins
+TP_DATA_DIR := /usr/local/share/tuxpaint
+SOURCE_PATH := ./src/scratch.c
+ICON_PATH := ./resources/scratch.png
+SOUND_PATH := ./resources/scratch.wav
+PLUGIN_NAME := scratch.so
+CFLAGS := -I$(SDL_LIB_PATH) -D_GNU_SOURCE=1 -D_REENTRANT -I$(TP_LIB_PATH)
+BUILD_DIR := ./build
+
+build:
+	@echo "Compiling plugin"
+	mkdir $(BUILD_DIR)
+	$(CC) --shared $(CFLAGS) $(SOURCE_PATH) -o $(BUILD_DIR)/$(PLUGIN_NAME)
+	@echo "Done."
+
+install:
+	@echo "Installing plugin"
+	sudo cp $(BUILD_DIR)/$(PLUGIN_NAME) $(TP_PLUGIN_DIR)
+	sudo cp $(ICON_PATH) $(TP_DATA_DIR)/images/magic
+	sudo cp $(SOUND_PATH) $(TP_DATA_DIR)/sounds/magic
+	@echo "Done."
+
+clean:
+	@echo "Cleaning up build files"
+	rm -rf $(BUILD_DIR)
+	@echo "Done."
--- src/scratch.c
+++ src/scratch.c
@@ -35,16 +35,16 @@
 #include "SDL_image.h"
 #include "SDL_mixer.h"
 
-
 // Place to hold the sound effect
 Mix_Chunk * snd_effect;
 
 // Place to hold the current RGB color
 Uint8 scratch_r, scratch_g, scratch_b;
 
+
 // API version from which this plugin is built against
 Uint32 scratch_api_version(void){
-    return (TP_MAGIC_API_VERSION);
+    return(TP_MAGIC_API_VERSION);
 }
 
 
@@ -52,7 +52,126 @@ Uint32 scratch_api_version(void){
 // Loads the sound from TP's data directory.
 int scratch_init(magic_api * api){
     char fname[1024];
-    sprintf(fname, "%s/sounds/magic/scratch.wav", api->data_directory);
+    sprintf(fname, "%s/sounds/magic/scratch.wav", 
+            api->data_directory);
     snd_effect = Mix_LoadWAV(fname);
-    return (snd_effect != NULL);
+    return(snd_effect != NULL); // Success ?
+}
+
+
+// Tell TP that we have only one tool
+int scratch_get_tool_count(magic_api * api){
+    return(1);
+}
+
+
+// Load plugin Icon
+SDL_Surface * scratch_get_icon(magic_api * api, int which){
+    char fname[1024];
+    sprintf(fname, "%s/images/magic/scratch.png", 
+            api->data_directory);
+    return(IMG_Load(fname)); // Return the icon
+}
+
+
+// Give Tux Paint our tool's name
+char * ex_get_name(magic_api * api, int which){
+    return(strdup("Scratch"));
+}
+
+
+// Give Tux Paint our tool's description
+char * scratch_get_description(magic_api * api, int which){
+    return(strdup("Click here and drag to make a scratched effect"));
+}
+
+
+// We are not using color palette
+int scratch_requires_colors(magic_api * api, int which){
+    return(0);
+}
+
+
+// Clean up after ourselves when TP quits.
+void scratch_shutdown(magic_api * api){
+    // Release RAM used by our sfx
+    Mix_FreeChunk(snd_effect);
+}
+
+
+// Respond to clicks (mouse button down event)
+void scratch_click(magic_api * api, int which,
+        SDL_Surface * canvas,
+        SDL_Surface * snapshot,
+        int x, int y,
+        SDL_Rect * update_rect){
+    // In our case a click is same as drag. 
+    // So send the same x, y to drag.
+    scratch_drag(api, which, canvas, snapshot,
+            x, y, x, y, update_rect);
+}
+
+
+// Respond to drags (mouse motion events while
+// user is clicking)
+void scratch_drag(magic_api * api, int which,
+        SDL_Surface * canvas,
+        SDL_Surface * snapshot,
+        int ox, int oy, int x, int y,
+        SDL_Rect * update_rect){
+    // Tell Tux Paint to calculate a line between
+    // the two points (ox,oy) and (x,y),
+    // calling our callback function every step
+    api->line((void *) api, which,
+            canvas, snapshot,
+            ox, oy, x, y, 1, // old, new coordinates and step
+            do_scratch); // do the scratch effect
+    // Tell TP what part of canvas was changed.
+    // We'll take top-left and bottom-right.
+    if (ox > x) { int tmp=ox; ox=x; x=tmp; }
+    if (oy > y) { int tmp=oy; oy=y; y=tmp; }
+
+    // Fill in the (x,y) and (w,h) elements of
+    // the update rectangle for Tux Paint
+    // (Our brush is 9x9, centered around (x,y))
+    update_rect->x = ox - 4;
+    update_rect->y = oy - 4;
+    update_rect->w = (x + 4) - update_rect->x;
+    update_rect->h = (y + 4) - update_rect->y;
+
+    // Play the sound
+    api->playsound(snd_effect,
+            (x * 255) / canvas->w,
+            255);
+}
+
+
+// Respond to releases (mouse button up event)
+void scratch_release(magic_api * api, int which,
+        SDL_Surface * canvas,
+        SDL_Surface * snapshot,
+        int x, int y,
+        SDL_Rect * update_rect){
+} // Nothing to do here
+
+
+// Do the actual effect
+void do_scratch(void * ptr, int which,
+        SDL_Surface * canvas,
+        SDL_Surface * snapshot,
+        int x, int y){
+    // Need to cast, since we get a void ptr
+    magic_api * api = (magic_api *) ptr;
+    
+    // Quick-and-dirty 9x9 round brush
+    for (int yy = -4; yy <= 4; yy++){
+        for (int xx = -4; xx <= 4; xx++){
+            if (api->in_circle(xx, yy, 4)){ // Round?
+                // Ask TP to change the canvas...
+                api->putpixel(canvas, x+xx, y+yy,
+                        SDL_MapRGB(canvas->format,
+                            ex_r, ex_g, ex_b));
+            }
+        }
+    }
 }
